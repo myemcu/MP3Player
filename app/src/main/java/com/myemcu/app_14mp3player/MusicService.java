9413fad341162b338b99c10708f0d31c4c3e5e5a
myemcu <1142497680@qq.com>
2016-08-09 17:39:31 +0800

    V3，Service框架实现。

--- app/src/main/java/com/myemcu/app_14mp3player/MusicService.java
+++ app/src/main/java/com/myemcu/app_14mp3player/MusicService.java
@@ -1,7 +1,12 @@
 package com.myemcu.app_14mp3player;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
 public class MusicService extends Service {
@@ -11,6 +16,29 @@ public class MusicService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+
+        AssetManager am = getAssets(); // 获取资源管(资源管理器)
+
+        // 在意图过滤器中注册其广播
+        ServiceReceiver serviceReceiver = new ServiceReceiver();
+        IntentFilter filter = new IntentFilter(MainActivity.CTRL);
+        registerReceiver(serviceReceiver,filter);
+
+        MediaPlayer mPlayer = new MediaPlayer();
+
+        super.onCreate();
+    }
+
+    private class ServiceReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
     }
 }
