87c2e8559a5694d7bf63a2c901582e144fe41715
myemcu <1142497680@qq.com>
2016-08-10 14:16:12 +0800

    V4，完成版。

--- app/src/main/java/com/myemcu/app_14mp3player/MainActivity.java
+++ app/src/main/java/com/myemcu/app_14mp3player/MainActivity.java
@@ -1,3 +1,9 @@
+/*
+* 程序异常：
+*           1. Activity与Service的广播通信间的标识不一致导致通信不上；
+*           2. Activity中的IntentFilter filter = new IntentFilter(广播标识);中必须写上广播标识。
+* */
+
 package com.myemcu.app_14mp3player;
 
 import android.content.BroadcastReceiver;
@@ -7,8 +13,12 @@ import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.xml.transform.Source;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageButton mPlay;
     private TextView mTitle;
     private TextView mAuthor;
+    private Button mPrev;
+    private Button mNext;
 
     public static final String CTRL = "com.myemcu.app_14mp3player.ctrl"; // 播放，暂停控制
     public static final String UPTE = "com.myemcu.app_14mp3player.upte"; // 更新，界面显示
@@ -25,8 +37,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String[] titleStr  = new String[] {"老男孩","春天里","在路上"};
     String[] authorStr = new String[] {"筷子兄弟","汪峰","刘欢"};
 
+    private ActivityReceiver mActivityReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) { // 主框架搭建
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -35,11 +50,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 集中处理Button点击事件
         mStop.setOnClickListener(this); // Alt+Enter(在MainActivity上实现接口)
         mPlay.setOnClickListener(this);
+        mPrev.setOnClickListener(this);
+        mNext.setOnClickListener(this);
 
-        // 创建自定义广播接收对象并在意图过滤器中注册
-        ActivityReceiver activityReceiver = new ActivityReceiver(); // Alt+Enter(内部类)
-        IntentFilter filter = new IntentFilter();
-        registerReceiver(activityReceiver,filter);
+        // 在意图过滤器中注册其广播
+        // Alt+Enter(内部类)
+        mActivityReceiver = new ActivityReceiver();
+        IntentFilter filter = new IntentFilter(UPTE); // 记得里面有参数
+        registerReceiver(mActivityReceiver,filter);
 
         // 启动后台Service(先在工程中建立该Service)
         Intent intent = new Intent(this,MusicService.class);
@@ -51,17 +69,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mPlay = (ImageButton) findViewById(R.id.play);
         mTitle = (TextView) findViewById(R.id.title);
         mAuthor = (TextView) findViewById(R.id.author);
+        mPrev = (Button) findViewById(R.id.prev);
+        mNext = (Button) findViewById(R.id.next);
     }
 
     @Override
     public void onClick(View v) { // Button事件的集中处理
 
+        Intent intent = new Intent(CTRL);
+
+        switch (v.getId()) {
+            case R.id.play: intent.putExtra("ctl",1); break;
+            case R.id.stop: intent.putExtra("ctl",2); break;
+            case R.id.prev: intent.putExtra("ctl",3); break;
+            case R.id.next: intent.putExtra("ctl",4); break;
+        }
+
+        sendBroadcast(intent);  // 发送广播并携带相应数据
     }
 
-    private class ActivityReceiver extends BroadcastReceiver{
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mActivityReceiver);
+    }
+
+    //Activity的广播接收器主要实现界面更新
+    private class ActivityReceiver extends BroadcastReceiver{ // Alt+Enter实现方法
         @Override
         public void onReceive(Context context, Intent intent) {
 
-        } // Alt+Enter实现方法
+            int current = intent.getIntExtra("current",-1);
+            int update  = intent.getIntExtra("update",-1);
+
+            if (current>=0) {
+                mTitle.setText(titleStr[current]);
+                mAuthor.setText(authorStr[current]);
+            }
+
+            switch (update) {
+                case 0x11: mPlay.setImageResource(R.drawable.play);  status=0x11; break;
+                case 0x12: mPlay.setImageResource(R.drawable.pause); status=0x12; break;
+                case 0x13: mPlay.setImageResource(R.drawable.play);  status=0x13; break;
+            }
+
+        }
     }
 }
--- app/src/main/java/com/myemcu/app_14mp3player/MusicService.java
+++ app/src/main/java/com/myemcu/app_14mp3player/MusicService.java
@@ -5,11 +5,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MusicService extends Service {
+
+    int cnt=0; // 当前曲号
+
+    String[] music = new String[] {"oldboy.mp3","spring.mp3","way.mp3"};
+
+    private AssetManager mAssets;
+    private MediaPlayer mPlayer;
+
+    int status = 0x11;
+    private ServiceReceiver mServiceReceiver;
+
     public MusicService() {
     }
 
@@ -23,22 +38,107 @@ public class MusicService extends Service {
     @Override
     public void onCreate() {
 
-        AssetManager am = getAssets(); // 获取资源管(资源管理器)
+        // 获取资源管(资源管理器)
+        mAssets = getAssets();
 
         // 在意图过滤器中注册其广播
-        ServiceReceiver serviceReceiver = new ServiceReceiver();
+        mServiceReceiver = new ServiceReceiver();
         IntentFilter filter = new IntentFilter(MainActivity.CTRL);
-        registerReceiver(serviceReceiver,filter);
+        registerReceiver(mServiceReceiver,filter);
 
-        MediaPlayer mPlayer = new MediaPlayer();
+        // 创建媒体播放器
+        mPlayer = new MediaPlayer();
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {  //开机顺序播放(默认)
+
+                cnt++;
+                if (cnt>=3) cnt=0;
+
+                Intent intent = new Intent(MainActivity.UPTE);
+                intent.putExtra("current",cnt);
+                sendBroadcast(intent); // 发送广播到MainActivity
+
+                preparePlay(music[cnt]); // 音乐播放
+            }
+        });
 
         super.onCreate();
     }
 
+    // 音乐播放(方法)
+    private void preparePlay(String s) {
+        try {
+                AssetFileDescriptor afd = mAssets.openFd(s); // 打开指定的音乐文件
+                mPlayer.reset();
+                mPlayer.setDataSource(afd.getFileDescriptor(),afd.getStartOffset(),afd.getLength());
+                mPlayer.prepare();
+                mPlayer.start();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private class ServiceReceiver extends BroadcastReceiver{
         @Override
-        public void onReceive(Context context, Intent intent) {
+        public void onReceive(final Context context, Intent intent) {
 
+            int control = intent.getIntExtra("ctl",-1);
+
+            switch (control) {
+
+                case 1: if (status==0x11) {
+                            preparePlay(music[cnt]);
+                            status=0x12;
+                        }
+                        else if (status==0x12) {
+                            mPlayer.pause();
+                            status=0x13;
+                        }
+                        else if (status==0x13) {
+                            mPlayer.start();
+                            status=0x12;
+                        }
+                        break;
+
+                case 2: if (status==0x12 || status==0x13) {
+                            mPlayer.stop();
+                             status=0x11;
+                        }
+
+                        break;
+
+                case 3: if (status==0x12) {
+                            cnt--;
+                            if (cnt<0)
+                            cnt=2;
+                            preparePlay(music[cnt]);
+                        }
+                        break;
+
+                case 4: if (status==0x12) {
+                            cnt++;
+                            if (cnt>=3)
+                            cnt=0;
+                            preparePlay(music[cnt]);
+                        }
+                        break;
+            }
+
+            //preparePlay(music[cnt]); // 放在此处歌就停不下来
+
+            /* 发送广播通知Activity更改图标、文本框 */
+            Intent sendIntent = new Intent(MainActivity.UPTE);
+            sendIntent.putExtra("update",status);
+            sendIntent.putExtra("current",cnt);
+            sendBroadcast(sendIntent);
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mServiceReceiver);
     }
 }
